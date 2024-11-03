6de0ed47ad86b3a4cdc09e454027fd27b15a5799
Joshua Terry <giodev1999@gmail.com>
2024-11-03 21:39:42 +0000

    Remove redundant mod entries in mods.tsx
    Eliminated duplicate mod entries with identical 'modrinthid' values to simplify the component data structure. This reduction will make the codebase cleaner and potentially improve data handling efficiency.

--- src/app/components/mods.tsx
+++ src/app/components/mods.tsx
@@ -3,12 +3,4 @@ export const Mods = [
     modrinthid: "ySCXsBkM",
     id: "00",
   },
-  {
-    modrinthid: "ySCXsBkM",
-    id: "01",
-  },
-  {
-    modrinthid: "ySCXsBkM",
-    id: "02",
-  },
 ];
